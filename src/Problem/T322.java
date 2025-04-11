c8173cdfcd5d36baa5697b6fbbb01b686bc8e03b
hahi <1249695730@qq.com>
2025-04-11 23:33:08 +0800

    T322

---
+++ src/Problem/T322.java
@@ -0,0 +1,21 @@
+package Problem;
+
+public class T322 {
+
+    final int INF = Integer.MAX_VALUE;
+
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i ++)
+            dp[i] = INF;
+        for (int i = 0; i < amount; i ++)
+            for (int j = 0; j < coins.length; j ++)
+                if (dp[i] != INF && (long) i + coins[j] <= amount)
+                    dp[i + coins[j]] = Math.min(dp[i + coins[j]], dp[i] + 1);
+
+        return dp[amount] == INF ? -1 : dp[amount];
+    }
+
+}
