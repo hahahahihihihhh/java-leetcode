b1617259a0600522f715b4e4bc8e68aac96b21b8
hahi <ox8cccccccc@163.com>
2025-04-20 16:56:39 +0800

    opt

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
-import Problem.T438;
-import Problem.T80;
+import java.util.*;
+import java.math.*;
 
 public class Main {
+    static int base = 10;
+
     public static void main(String[] args) {
-//        String s = "abab", p = "ab";
-//        T438 t = new T438();
-//        t.findAnagrams(s, p);
-        String a = "aa" + 111;
-        System.out.println(a.charAt(0) + 2);
-        System.out.println(a);
+        Stack<Integer> stk = new Stack<>(); // push pop peek isEmpty size clear
+        Map<Integer, Integer> hmp = new HashMap<>();     // get put remove isEmpty size clear entrySet Map.entry
+        Map<Integer, Integer> tmp = new TreeMap<>((o1, o2) -> Integer.compare(o2, o1)); // (o1, o2) -> Integer.compare(o2, o1)
+
+        Queue<Integer> q = new LinkedList<>();          // offer poll peek isEmpty size clear
+        Queue<Integer> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o2, o1));      // (o1, o2) -> Integer.compare(o2, o1)
+
+        Set<Integer> hs = new HashSet<>();      // contains add remove isEmpty size clear
+        Set<Integer> ts = new TreeSet<>((o1, o2) -> Integer.compare(o2, o1));   // (o1, o2) -> Integer.compare(o2, o1)
+
+        stk.add(1);
+        stk.add(2);
+        System.out.println(stk.empty() + " " + stk.isEmpty());
+
+        int x = 4, y = 2;
+        BigInteger bx = BigInteger.valueOf(x), by = BigInteger.valueOf(y);
+        int gcd = bx.gcd(by).intValue();
+        System.out.println(bx + " " + by + " " + gcd);
+
+        assert stk.empty() != stk.isEmpty();
+//        System.out.println(stk.search(111));
+        return ;
+//        Map<Integer, Integer> mp;
+//        mp.
+
     }
 }
