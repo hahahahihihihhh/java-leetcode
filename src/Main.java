d3df3c23535b13dcc80e1ea11a50f23d0f48fe27
hahi <1249695730@qq.com>
2025-04-20 16:55:39 +0800

    opt

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,52 @@
-import Problem.T438;
-import Problem.T80;
+import javax.imageio.IIOException;
+import java.io.*;
+import java.sql.SQLOutput;
 
 public class Main {
-    public static void main(String[] args) {
-//        String s = "abab", p = "ab";
-//        T438 t = new T438();
-//        t.findAnagrams(s, p);
-        String a = "aa" + 111;
-        System.out.println(a.charAt(0) + 2);
-        System.out.println(a);
+
+    static class MyClass implements Serializable {
+        NestedClass nestedClass;
+
+        MyClass(int x) {
+            this.nestedClass = new NestedClass(x);
+        }
+
+        public void print() {
+            System.out.println(nestedClass.nestedField);
+        }
+
+        public MyClass deepCopy() {
+            try {
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                ObjectOutputStream oos = new ObjectOutputStream(bos);
+                oos.writeObject(this);
+                oos.flush();
+                oos.close();
+
+                ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+                ObjectInputStream ois = new ObjectInputStream(bis);
+                return (MyClass) ois.readObject();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
     }
+
+    static class NestedClass implements Serializable {
+        int nestedField;
+
+        NestedClass(int x) {
+            this.nestedField = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyClass copy = new MyClass(2);
+        copy.deepCopy().print();
+
+    }
+
 }
